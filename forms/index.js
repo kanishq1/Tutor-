9826d38d3ebbf489fada2ba3c950f6967f9d71a2
Kanishq <kanishq@spennyapp.com>
2020-07-09 12:21:32 +0530

    form to sheet

---
+++ forms/index.js
@@ -0,0 +1,14 @@
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Array.from(formData.entries()).reduce(
+    (memo, pair) => ({
+      ...memo,
+      [pair[0]]: pair[1],
+    }),
+    {}
+  );
+  console.log(data);
+  axios.post('https://tutorplusbackend.herokuapp.com/register', data);
+});
